d5c1ce550e4b12486fbcabff6c02b5ee210949ea
Atrox493 <2577632081@qq.com>
2022-03-13 14:27:47 +0800

    TS基本类型-类型声明

---
+++ TypeScript Basic Grammar Learning/example02_basis.ts
@@ -0,0 +1,28 @@
+//声明一个变量a，同时指定它的类型为number
+let a: number;
+
+//a的类型设置为number,在以后的使用过程中a的值只能是数字
+a = 10;
+// a='hello'; 不能将类型“string”分配给类型“number”。
+
+let b: string;
+b = 'hello';
+
+//声明完变量直接进行赋值
+let c: boolean = false;
+
+//如果变量的声明和赋值是同时进行的，TS可以自动对变量进行类型检测
+let c2 = false;
+//c2 = 'c2' 不能将类型“string”分配给类型“boolean”。
+
+//js中的函数是不考虑参数的类型和个数的
+function sum1(a, b) {
+    return a + b;
+}
+sum1(123, '456');//'123456'
+
+function sum2(a: number, b: number): number {
+    return a + b;
+}
+// sum2(123,'456');//类型“string”的参数不能赋给类型“number”的参数。
+// sum2(123,456,789);//应有 2 个参数，但获得 3 个。
